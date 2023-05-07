38e06e5d6cd15af177efdfc0b4a60ef27dfb653a
xavier-fenton <xavierfenton99@gmail.com>
2023-05-07 20:25:46 +1200

    basic cmd promt options with if statements

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-
+using namespace std;
 
 // https://www.techiedelight.com/print-contents-array-cpp/ help with arrays
+
 int main()
 {
-  int nums[] = {0, 0, 0, 0, 0, 0};
-  size_t n = sizeof(nums)/sizeof(nums[0]);
-  for (size_t i = 0; i < n; i++){
-    std::cout << nums[i] << std::endl;
-
+  char input;
+  string close = "Close";
+  string running = "Programming is running";
+  cout << "[R]un the program \n\[C]lose the program: "; 
+  cin >> input; 
+  if(input == 'R'){
+    std::cout << running <<std::endl;
   }
+  else if(input == 'C'){
+    std::cout << close <<std::endl;
+  }
+  // int nums[] = {0, 0, 0, 0, 0, 0};
+
+  // size_t n = sizeof(nums)/sizeof(nums[0]);
+  // for (size_t i = 0; i < n; i++){
+  //   std::cout << nums[i] << std::endl;
+
+  // }
   
 }
